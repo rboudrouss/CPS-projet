4df253a3a4467d01c070ec69f07bdedf33d54add
rboudrouss <r.boudrouss@gmail.com>
2025-02-17 10:43:56 +0100

    endpoint working ??

--- src/com/rboud/cps/core/DHTEndpoint.java
+++ src/com/rboud/cps/core/DHTEndpoint.java
@@ -1,43 +1,39 @@
 package com.rboud.cps.core;
 
+import fr.sorbonne_u.components.endpoints.EndPoint;
 import fr.sorbonne_u.components.endpoints.EndPointI;
+import fr.sorbonne_u.components.endpoints.POJOEndPoint;
 import fr.sorbonne_u.cps.dht_mapreduce.interfaces.content.ContentAccessSyncI;
 import fr.sorbonne_u.cps.dht_mapreduce.interfaces.endpoints.ContentNodeBaseCompositeEndPointI;
 import fr.sorbonne_u.cps.dht_mapreduce.interfaces.mapreduce.MapReduceSyncI;
 
 public class DHTEndpoint implements ContentNodeBaseCompositeEndPointI<ContentAccessSyncI, MapReduceSyncI> {
-  private EndPointI<ContentAccessSyncI> contentAccessEndPoint;
-  private EndPointI<MapReduceSyncI> mapReduceEndPoint;
-
-  private boolean clientSideInitialise = false;
+  private EndPoint<ContentAccessSyncI> contentAccessEndPoint;
+  private EndPoint<MapReduceSyncI> mapReduceEndPoint;
 
   public DHTEndpoint() {
-    this.contentAccessEndPoint = null;
-    this.mapReduceEndPoint = null;
+    this.contentAccessEndPoint = new POJOEndPoint<>(ContentAccessSyncI.class);
+    this.mapReduceEndPoint = new POJOEndPoint<>(MapReduceSyncI.class);
   }
 
-  @SuppressWarnings("unchecked")
   @Override
   public void initialiseServerSide(Object serverSideEndPointOwner) {
-    if (serverSideEndPointOwner instanceof ContentAccessSyncI)
-      this.contentAccessEndPoint = (EndPointI<ContentAccessSyncI>) serverSideEndPointOwner;
-    
-    if (serverSideEndPointOwner instanceof MapReduceSyncI)
-      this.mapReduceEndPoint = (EndPointI<MapReduceSyncI>) serverSideEndPointOwner;
-    
+    assert serverSideEndPointOwner != null;
+
+    this.contentAccessEndPoint.initialiseServerSide(serverSideEndPointOwner);
+    this.mapReduceEndPoint.initialiseServerSide(serverSideEndPointOwner);
+
     assert serverSideInitialised();
-    assert clientSideInitialised();
   }
 
-  /*
-   * Note that this is the same as the server side
-   */
   @Override
   public void initialiseClientSide(Object clientSideEndPointOwner) {
     assert clientSideEndPointOwner != null;
     assert serverSideInitialised();
 
-    this.clientSideInitialise = true;
+    this.contentAccessEndPoint.initialiseClientSide(clientSideEndPointOwner);
+    this.mapReduceEndPoint.initialiseClientSide(clientSideEndPointOwner);
+
     assert clientSideInitialised();
   }
 
@@ -49,9 +45,9 @@ public class DHTEndpoint implements ContentNodeBaseCompositeEndPointI<ContentAcc
   @SuppressWarnings("unchecked")
   @Override
   public <I, J extends I> EndPointI<J> getEndPoint(Class<I> inter) {
-    if (inter.equals(ContentAccessSyncI.class)) {
+    if (ContentAccessSyncI.class.isAssignableFrom(inter)) {
       return (EndPointI<J>) contentAccessEndPoint;
-    } else if (inter.equals(MapReduceSyncI.class)) {
+    } else if (MapReduceSyncI.class.isAssignableFrom(inter)) {
       return (EndPointI<J>) mapReduceEndPoint;
     }
     return null;
@@ -59,24 +55,24 @@ public class DHTEndpoint implements ContentNodeBaseCompositeEndPointI<ContentAcc
 
   @Override
   public boolean serverSideInitialised() {
-    return complete();
+    return complete() && this.contentAccessEndPoint.serverSideInitialised() && this.mapReduceEndPoint.serverSideInitialised();
   }
 
   @Override
   public boolean clientSideInitialised() {
-    return this.clientSideInitialise && serverSideInitialised();
+    return complete() && this.contentAccessEndPoint.clientSideInitialised() && this.mapReduceEndPoint.clientSideInitialised() && this.serverSideInitialised();
   }
 
   @Override
   public void cleanUpServerSide() {
-    this.contentAccessEndPoint = null;
-    this.mapReduceEndPoint = null;
+    this.contentAccessEndPoint.cleanUpServerSide();
+    this.mapReduceEndPoint.cleanUpServerSide();
   }
 
   @Override
   public void cleanUpClientSide() {
-    this.contentAccessEndPoint = null;
-    this.mapReduceEndPoint = null;
+    this.contentAccessEndPoint.cleanUpClientSide();
+    this.mapReduceEndPoint.cleanUpClientSide();
   }
 
   @Override
--- src/com/rboud/cps/main/Main.java
+++ src/com/rboud/cps/main/Main.java
@@ -18,10 +18,11 @@ public class Main {
     endpoint.initialiseClientSide(facade);
 
     try {
+      Personne p4 = Personne.getRandomPersonne();
       facade.put(new Id(1), Personne.getRandomPersonne());
       facade.put(new Id(2), Personne.getRandomPersonne());
       facade.put(new Id(3), Personne.getRandomPersonne());
-      facade.put(new Id(4), Personne.getRandomPersonne());
+      facade.put(new Id(4), p4);
       facade.put(new Id(5), Personne.getRandomPersonne());
       facade.put(new Id(6), Personne.getRandomPersonne());
       facade.put(new Id(7), Personne.getRandomPersonne());
@@ -29,12 +30,16 @@ public class Main {
       facade.put(new Id(9), Personne.getRandomPersonne());
       facade.put(new Id(10), Personne.getRandomPersonne());
 
-      Personne p = (Personne) facade.get(new Id(1));
-      System.out.println(p);
+      node.printChain("DEBUG");
+
+      Personne p = (Personne) facade.get(new Id(4));
+      System.out.println("FOUND at id 4:" + p);
+      System.out.println("Should be equal to p4: " + p4);
 
       Integer out = facade.mapReduce((_) -> true, (e) -> e.getValue("AGE"), (a, n) -> (a + (Integer) n),
           (a, n) -> (a + n), 0);
       System.out.println("somme age : " + out);
+
     } catch (Exception e) {
       e.printStackTrace();
       return;
