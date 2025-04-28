fdafcd8397b244a46d550528ef2d13d1820d19bc
rboudrouss <r.boudrouss@gmail.com>
2025-04-28 13:48:43 +0200

    don't generate & include docs

--- zip.sh
+++ zip.sh
@@ -1,3 +1,2 @@
-./generateDocs.sh
 ./clearLogs.sh
-7z a Boudrouss-CPS.zip src .vscode .gitignore .git docs logs
+7z a Boudrouss-CPS-final.zip src .vscode .gitignore .git logs generateDocs.sh clearLogs.sh zip.sh
